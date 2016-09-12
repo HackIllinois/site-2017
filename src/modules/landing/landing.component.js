374507318a31fbdd0160ca5f576b5892820a2b6b
Achal Varma <achalv2.0@gmail.com>
2016-09-12 02:40:23 -0500

    scaffolding

--- src/modules/landing/landing.component.js
+++ src/modules/landing/landing.component.js
@@ -1,20 +1,17 @@
 import React from 'react'
 
+import common from '../common/common.css'
 import styles from './landing.styl'
 
 const Landing = () => (
-	<div className={styles.landing}> 
-		<div className={styles.logo}> [logo placeholder] </div>
-		<div className={styles.cave}> 
-			<span className={styles.caveText}> [cave placeholder] </span>
-		</div>
-		<div className={styles.formWrapper}> 
-			<div className={styles.title}> Dig Deeper! </div>
-			<div className={styles.date}> Feb. 17 - 20, 2017 </div>
-			<div className={styles.formText}> Be the first to know when the adventure begins. </div>
-			<input id={styles.registrationInput} placeholder="future-adventurer@underground.com"/>
-		</div>
-	</div>
+  <div className={styles.landing}> 
+    <div className={styles.cave}> 
+      <img src="src/assets/landing-cave.svg"/>
+      <div className={styles.logo}>
+        <img src="src/assets/landing-sign.svg"/>
+      </div>
+    </div>
+  </div>
 )
 
 export default Landing
