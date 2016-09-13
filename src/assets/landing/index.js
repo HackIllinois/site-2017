1d54f5b8884590501b2685e16e4c00eaf0d72ece
Vishrut Dixit <vishrutdixit@gmail.com>
2016-09-12 21:48:19 -0500

    yay for images

---
+++ src/assets/landing/index.js
@@ -0,0 +1,6 @@
+import Cave from './landing-cave.svg'
+import Eyes from './landing-eyes.svg'
+import Sign from './landing-sign.svg'
+import Logo from './landing-logo.svg'
+
+export {Cave, Eyes, Sign, Logo}
--- src/modules/landing/landing.component.js
+++ src/modules/landing/landing.component.js
@@ -1,15 +1,20 @@
 import React from 'react'
 
+
 import common from '../common/common.css'
 import styles from './landing.styl'
 
+import {Cave, Eyes, Sign, Logo} from '../../assets/landing'
+
 const Landing = () => (
   <div className={styles.landing}> 
-    <div className={styles.cave}> 
-      <img src="src/assets/landing-cave.svg"/>
-      <div className={styles.logo}>
-        <img src="src/assets/landing-sign.svg"/>
+    <div className={styles.cave}>
+      <img src={Cave}/>
+      <div className={styles.sign}>
+        <img src={Sign}/>
       </div>
+    </div>
+    <div className={styles.caveMobileOnly}>
     </div>
   </div>
 )
