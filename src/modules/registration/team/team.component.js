207a78a00579b4b127fe43239f54c69adb7732b8
Sujay Khandekar <khandek2@illinois.edu>
2017-01-25 14:23:05 -0600

    More UI buttons for back and finish on team page

--- src/modules/registration/team/team.component.js
+++ src/modules/registration/team/team.component.js
@@ -10,14 +10,21 @@ class Team extends Component {
 	render = () => (
 		<div className={styles.container}>
 			<div className={styles['title-text']}> TEAM </div>
-			<div className={styles['instruction-text']}> Interested in working with a team? Let us know who you prefer to work with via their GitHub username. We don't have team size limits however we may not be able to accommodate your whole team. </div>
+			<div className={styles['instruction-text']}> Interested in working with a team? Let us know who you prefer to work with via their GitHub username. We don't have team size limits; however, we may not be able to accommodate your whole team. </div>
 			<div className={styles['team-select-container']}>
 				{this.props.store.collaborators.map((member)=>(
 					<Input value={member} id='team member' options={[]} />
 				))}
 				<Input id='add team member' options={[]}/>
 			</div>
-			<Button dest='/registration/5' arrow='right' label='FINISH'/>
+			<div className={styles['navigation-container']}>
+			    <div className={styles['back-button']}>
+			    	<Button dest='/registration/3' arrow='left' label='BACK'/>
+			    </div>
+			    <div className={styles['continue-button']}>
+			    	<Button dest='/registration/5' label='FINISH'/>
+			    </div>
+			</div>
 		</div>
 	)
 }
