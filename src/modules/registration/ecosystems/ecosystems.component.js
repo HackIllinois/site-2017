a3304dd9a802f1c206f211859e156a25bc527f18
Sujay Khandekar <khandek2@illinois.edu>
2017-01-25 22:35:42 -0600

    height changed to 60% for modal

--- src/modules/registration/ecosystems/ecosystems.component.js
+++ src/modules/registration/ecosystems/ecosystems.component.js
@@ -44,7 +44,7 @@ class Card extends Component {
 var Modal = require('boron/DropModal')
 var modalStyle = {
 	width: '100%',
-	height: '100%',
+	height: '60%',
 	backgroundColor: '#122342',
 	border: '5px solid #57C7DF',
 	padding: '45px'
