ff996c6cb360f8a90f859e681db116832c87fabb
Yasha Mostofi <yasha.mostofi@gmail.com>
2017-01-27 05:20:13 -0600

    Well.

--- src/modules/registration/stores/index.js
+++ src/modules/registration/stores/index.js
@@ -6,17 +6,24 @@ import {browserHistory} from 'react-router'
 const loggedIn = window.location.href.includes('code')
 const code = loggedIn ?  window.location.search.slice(6) : ''
 
-const ab2str = (buf) => {
-    return String.fromCharCode.apply(null, new Uint8Array(buf));
+function arrayBufferToBase64( buffer ) {
+    var binary = '';
+    var bytes = new Uint8Array( buffer );
+    var len = bytes.byteLength;
+    for (var i = 0; i < len; i++) {
+        binary += String.fromCharCode( bytes[ i ] );
+    }
+    return window.btoa( binary );
 }
 
-const str2ab = (str) => {
-  var buf = new ArrayBuffer(str.length*1); // 2 bytes for each char
-  var bufView = new Uint8Array(buf);
-  for (var i=0, strLen=str.length; i<strLen; i++) {
-    bufView[i] = str.charCodeAt(i);
-  }
-  return buf;
+function base64ToArrayBuffer(base64) {
+    var binary_string =  window.atob(base64);
+    var len = binary_string.length;
+    var bytes = new Uint8Array( len );
+    for (var i = 0; i < len; i++)        {
+        bytes[i] = binary_string.charCodeAt(i);
+    }
+    return bytes.buffer;
 }
 
 class RegistrationStore {
@@ -48,64 +55,69 @@ class RegistrationStore {
         const submitToken = fromPromise(axios.post('https://api.hackillinois.org/v1/registration/attendee', req, config))
                     
         when(() => submitToken.state !== 'pending',
-             () => {    
-                        console.log('finish')
-                        
-                        const config = {
-                            headers: {
-                                'Authorization': localStorage.getItem('authorization'),
-                                'Content-Type': 'application/pdf',
-                            }
-                        };
-
-                        const resumeToken = fromPromise(axios.post('https://api.hackillinois.org/v1/upload/resume', str2ab(localStorage.getItem('resume')), config));
-                    
-                        when(() => resumeToken.state !== 'pending',
-                             () => { 
-                                    if(resumeToken.state !== 'rejected') window.location = '/registration/5'
-                            });
-                            
-                        }); 
-                        
-
-
+             () => {                    
+                const config = {
+                    headers: {
+                        'Authorization': localStorage.getItem('authorization'),
+                        'Content-Type': 'application/pdf',
+                    }
+                };
+                const resumeToken = fromPromise(axios.post('https://api.hackillinois.org/v1/upload/resume', base64ToArrayBuffer(localStorage.getItem('resume')), config));
+                when(() => resumeToken.state !== 'pending',
+                     () => { 
+                            if(resumeToken.state !== 'rejected') window.location = '/registration/5'
+                });            
+        }); 
     }
-    saveAttendee = (attendeeData) => {
-
-        
-
-        localStorage.setItem('attendee', JSON.stringify(attendeeData))
-
-        const authToken = fromPromise(axios.post('https://api.hackillinois.org/v1/auth', {'email': 'systems@hackillinois.org', 'password': 'W5FHacHWmMwXcyxAajT'}));
-        when(() => authToken.state !== 'pending',() => {
-            if(authToken.state !== 'rejected') {
-
-            this.adminAuth = authToken.value.data.data.auth
-            
-            const config = {
-                headers: {
-                    'Authorization': this.adminAuth,
-                    'Content-Type': 'application/json'
-                }
-            };
-
-            //Create new User
-            const userToken = fromPromise(axios.post('https://api.hackillinois.org/v1/user', {'email': this.userData.email, 'password': this.userData.createPassword }, config));
-        
-            when(() => userToken.state !== 'pending',() => {
-                if(userToken.state !== 'rejected') {
-                    
-                    this.userAuth = userToken.value.data.data.auth
-                    
-                    localStorage.setItem('authorization', this.userAuth)
-                    console.log(ab2str(this.userData.resume))
-                    localStorage.setItem('resume', ab2str(this.userData.resume))
-
-                    //window.location = '/registration/3'
-
-                    }});
+   saveAttendee = (attendeeData) => {
+    localStorage.setItem('attendee', JSON.stringify(attendeeData))
+    const authToken = fromPromise(axios.post('https://api.hackillinois.org/v1/auth', {'email': 'systems@hackillinois.org', 'password': 'W5FHacHWmMwXcyxAajT'}));
+    when(() => authToken.state !== 'pending',() => {
+        if(authToken.state !== 'rejected') {
+        this.adminAuth = authToken.value.data.data.auth
+        const config = {
+            headers: {
+                'Authorization': this.adminAuth,
+                'Content-Type': 'application/json'
+            }
+        };
+        //Create new User
+        const userToken = fromPromise(axios.post('https://api.hackillinois.org/v1/user', {'email': this.userData.email, 'password': this.userData.createPassword }, config));
+        when(() => userToken.state !== 'pending',() => {
+            if(userToken.state !== 'rejected') {
+                this.userAuth = userToken.value.data.data.auth
+                localStorage.setItem('authorization', this.userAuth)
+                localStorage.setItem('resume', arrayBufferToBase64(this.userData.resume))
+                // window.file = this.userData.resume
+                window.location = '/registration/3'
                 }});
+            }});
     }
+    /*
+    saveAttendee = (attendeeData) => {
+        localStorage.setItem('attendee', JSON.stringify(attendeeData))
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+
+        //Create new User
+        const userToken = fromPromise(axios.post('https://api.hackillinois.org/v1/user', {'email': this.userData.email, 'password': this.userData.createPassword }, config));
+    
+        when(() => userToken.state !== 'pending',() => {
+            if(userToken.state !== 'rejected') {
+                this.userAuth = userToken.value.data.data.auth
+                localStorage.setItem('authorization', this.userAuth)
+                window.hackillinois["upload"] = this.userData.resume
+                window.location = '/registration/3'
+            }
+            else {
+                console.log("issue");
+            }
+        });
+    }
+    */
 
     @observable adminAuth = ''
     @observable userAuth = ''
@@ -155,49 +167,6 @@ class RegistrationStore {
     @observable fileSize = 0;
     @observable collaborators = [];
 	@observable codeOfConductCheck = false;
-
-	
 }
 
 export default new RegistrationStore(code);
-
-
-
-/*
-constructor(code) {
-        /*
-        if(!window.location.pathname.includes('/registration/2')) return
-
-        //redirect
-        if(code == '') window.location = 'https://github.com/login/oauth/authorize?client_id=9674b582b52db2bf4197'
-
-        //console.log('not redirecting')
-        const token = fromPromise(axios.get('https://hackillinois-auth-helper.herokuapp.com/authenticate/'+code), 'initial')
-
-        when(
-            () => token.state !== 'pending',
-            () => {
-                if(token.state !== 'rejected') {
-                this.auth_token = token.value.data.token
-                const userInfo = fromPromise(
-                    axios.get('https://api.github.com/user', {
-                        headers: {'Authorization': 'token ' + this.auth_token}
-                    }))
-
-                when(
-                    () => userInfo.state !== 'pending',
-                    () => {
-                        if(userInfo.state == 'rejected') window.location = 'https://github.com/login/oauth/authorize?client_id=9674b582b52db2bf4197'
-                        this.isAuthenticated = true
-                        this.userData.github = userInfo.value.data.login
-                        this.userData.email = userInfo.value.data.email
-                        //this.userData.name = userInfo.value.data.name
-                    }
-                )
-            }
-        }
-        )
-        
-    }*/
-
-
