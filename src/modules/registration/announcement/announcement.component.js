10e6b87a145adfc3fa2c0e5d31c0a2c8457736c8
Sujay Khandekar <khandek2@illinois.edu>
2017-01-27 23:52:46 -0600

    removed email line from first reg page

--- src/modules/registration/announcement/announcement.component.js
+++ src/modules/registration/announcement/announcement.component.js
@@ -24,8 +24,7 @@ class Announcement extends Component {
             <div className={styles['announcement-text']}>
                 From February 24th to 26th, HackIllinois is focusing entirely on introducing developers to the Free and Open Source Community. Attendees will be able to choose between two tracks: <strong> Contribute </strong> or <strong> Create</strong>. <br/><br/>
                 In the <strong> Contribute </strong> track, attendees will be mentored while working on popular Open Source projects, providing a welcoming environment to make their first additions to the community. In the <strong> Create </strong> track, attendees will venture into the process of creating a brand new Open Source project and community, whether it is a fork, simply a script, or a new web framework. <br/><br/>
-                In making this shift, we've rebuilt our event format from the ground up to focus on collaboration and sustainability rather than competition. And as always, attendance is free, free as in gratis. <br/><br/>
-                If you have any issues or questions, send us an email at <a href="mailto:contact@hackillinois.org">contact@hackillinois.org</a>.
+                In making this shift, we've rebuilt our event format from the ground up to focus on collaboration and sustainability rather than competition. And as always, attendance is free, free as in gratis.
             </div>
             <Button onClick={this.clearStorage} label='REGISTER' dest='#' arrow='none' />
         </div>
