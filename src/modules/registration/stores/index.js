3749fc1a2d2c9bfe3abdfc7537e03d30277b7a83
Yasha Mostofi <yasha.mostofi@gmail.com>
2017-01-31 15:53:05 -0600

    sending JSON as error code

--- src/modules/registration/stores/index.js
+++ src/modules/registration/stores/index.js
@@ -61,7 +61,7 @@ class RegistrationStore {
             if(userToken.state == 'rejected') {
                 if (ga) {
                     ga('send', 'exception', {
-                        'exDescription': '/user : ' + userToken.value.response.data,
+                        'exDescription': '/user : ' + JSON.stringify(userToken.value.response.data.error),
                         'exFatal':true
                     })
                 }
@@ -93,7 +93,7 @@ class RegistrationStore {
                                     else {
                                         if (ga) {
                                             ga('send', 'exception', {
-                                                'exDescription': '/attendee : ' + submitToken.value.response.data, 
+                                                'exDescription': '/attendee : ' + JSON.stringify(submitToken.value.response.data.error), 
                                                 'exFatal': true
                                             })
                                         }
