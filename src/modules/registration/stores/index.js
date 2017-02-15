3dd6fc55e9869d5554be9b7d051a35a3b707e7df
Yasha Mostofi <yasha.mostofi@gmail.com>
2017-02-14 20:22:40 -0600

    Booleans

--- src/modules/registration/stores/index.js
+++ src/modules/registration/stores/index.js
@@ -103,7 +103,7 @@ class RegistrationStore {
         }
         if (sessionStorage.getItem('auth') == null && window.location.pathname != '/login'
             && window.location.pathname != '/' && window.location.pathname != '/projects'
-            && window.location.pathname = '/reset' && !window.location.pathname.includes('registration')) {
+            && window.location.pathname != '/reset' && !window.location.pathname.includes('registration')) {
             window.location = '/login'
         }
         if (sessionStorage.getItem('authorization') != null) {
