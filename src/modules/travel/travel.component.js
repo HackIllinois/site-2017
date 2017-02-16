e7edf8110f9f8c655c7707eefe0fb120ac062333
Simon Lu <lusimon95@gmail.com>
2017-02-15 23:25:32 -0600

    travel page styling updated

--- src/modules/travel/travel.component.js
+++ src/modules/travel/travel.component.js
@@ -6,7 +6,23 @@ import styles from './travel.scss'
 class Travel extends Component {
     render = () => (
         <div className={styles['travel']}>
-            Hello world
+
+            <div className={styles['travel-header']}>
+
+                <img src="src/assets/img/svg/landing-logo.svg" /> <br/><br />
+                <h3 className={styles['travel-header-title']}>Travel</h3>
+                <h5 className={styles['travel-header-subtitle']}>HackIllinois 2017 will be sending busses to the following schools!</h5>
+
+            </div>
+
+            <div className={styles['travel-body']}>
+
+                <div className={styles['travel-body-panel']}>
+                    <h4 className={styles['travel-body-panel-title']}></h4>
+
+                </div>
+
+            </div>
         </div>
     )
 }
