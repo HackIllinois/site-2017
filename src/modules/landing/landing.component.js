ee560850e957f80e9992d446ac824e04b0096421
Sujay Khandekar <khandek2@illinois.edu>
2017-02-16 23:04:02 -0600

    React way of rendering the sponsor list

--- src/modules/landing/landing.component.js
+++ src/modules/landing/landing.component.js
@@ -32,176 +32,75 @@ class Landing extends Component {
       this.setState({email: ""})
     });
   }
-  render = () => (
-    <div className={styles.landing}>
-      <div className={styles.lanterns}>
-        <div className={styles.lanternUnit}></div>
-        <div className={styles.lanternUnit}></div>
-        <div className={styles.lanternUnit}></div>
-        <div className={styles.lanternUnit}></div>
-        <div className={styles.lanternUnit}></div>
-        <div className={styles.lanternUnit}></div>
-        <div className={styles.lanternUnit}></div>
-      </div>
-      <div className={styles.hero}>
-        <img className={styles.logo} src="src/assets/img/landing-logo.png"/>
-        <div className={styles.dateLabel}>February 24th - 26th</div>
-        <div className={styles.host}>
-          Co-hosted by <a href="http://fulcrum-gt.com/">Fulcrum</a>
-        </div>
-        <div className={styles.register}>
-        <Button className={styles.mb20} register={true} dest='/registration' label='REGISTER NOW' />
-        <div className={styles.login} > Already registered? <a href='/login'> Log in </a> </div>
-        </div>
-      </div>
+  render = () =>  {
+      let sponsorUrls = [
+          "src/assets/img/home/sponsors/3red.svg",
+          "src/assets/img/home/sponsors/a16z.png",
+          "src/assets/img/home/sponsors/akuna.png",
+          "src/assets/img/home/sponsors/amadeus.svg",
+          "src/assets/img/home/sponsors/att.svg",
+          "src/assets/img/home/sponsors/bp.svg",
+          "src/assets/img/home/sponsors/capitalone.svg",
+          "src/assets/img/home/sponsors/citadel.svg",
+          "src/assets/img/home/sponsors/cloudera.svg",
+          "src/assets/img/home/sponsors/facebook.svg",
+          "src/assets/img/home/sponsors/firebase.png",
+          "src/assets/img/home/sponsors/github.png",
+          "src/assets/img/home/sponsors/huawei.svg",
+          "src/assets/img/home/sponsors/imo.svg",
+          "src/assets/img/home/sponsors/johndeere.svg",
+          "src/assets/img/home/sponsors/microsoft.svg",
+          "src/assets/img/home/sponsors/namecheap.png",
+          "src/assets/img/home/sponsors/onenorth.svg",
+          "src/assets/img/home/sponsors/qualtrics.svg",
+          "src/assets/img/home/sponsors/statefarm.svg",
+          "src/assets/img/home/sponsors/twilio.svg",
+          "src/assets/img/home/sponsors/viasat.svg",
+          "src/assets/img/home/sponsors/yahoo.svg"
+      ];
+      let sponsorList = sponsorUrls.map((url, key) => {
+            return (
+                <li className={styles.listItem} key={key}>
+                    <a className={styles.listItemLink} href="#">
+                        <img className={styles.listItemImage} src={url} />
+                    </a>
+                </li>
+            );
+      });
 
-      <h4 className={styles.sponsorHeading}>Sponsors</h4>
-      <div className={styles.sponsorContainer}>
-      <ul className={styles.list}>
-          <li className={styles.listItem}>
-              <a className={styles.listItemLink} href="#">
-                  <img className={styles.listItemImage} src="src/assets/img/home/sponsors/microsoft.svg" />
-              </a>
-          </li>
-          <li className={styles.listItem}>
-              <a className={styles.listItemLink} href="#">
-                  <img className={styles.listItemImage} src="src/assets/img/home/sponsors/imo.svg" />
-              </a>
-          </li>
-          <li className={styles.listItem}>
-              <a className={styles.listItemLink} href="#">
-                  <img className={styles.listItemImage} src="src/assets/img/home/sponsors/amadeus.svg" />
-              </a>
-          </li>
-          <li className={styles.listItem}>
-              <a className={styles.listItemLink} href="#">
-                  <img className={styles.listItemImage} src="src/assets/img/home/sponsors/att.svg" />
-              </a>
-          </li>
-          <li className={styles.listItem}>
-              <a className={styles.listItemLink} href="#">
-                  <img className={styles.listItemImage} src="src/assets/img/home/sponsors/capitalone.svg" />
-              </a>
-          </li>
-          <li className={styles.listItem}>
-              <a className={styles.listItemLink} href="#">
-                  <img className={styles.listItemImage} src="src/assets/img/home/sponsors/citadel.svg" />
-              </a>
-          </li>
-          <li className={styles.listItem}>
-              <a className={styles.listItemLink} href="#">
-                  <img className={styles.listItemImage} src="src/assets/img/home/sponsors/facebook.svg" />
-              </a>
-          </li>
-          <li className={styles.listItem}>
-              <a className={styles.listItemLink} href="#">
-                  <img className={styles.listItemImage} src="src/assets/img/home/sponsors/google.svg" />
-              </a>
-          </li>
-          <li className={styles.listItem}>
-              <a className={styles.listItemLink} href="#">
-                  <img className={styles.listItemImage} src="src/assets/img/home/sponsors/huawei.svg" />
-              </a>
-          </li>
-          <li className={styles.listItem}>
-              <a className={styles.listItemLink} href="#">
-                  <img className={styles.listItemImage} src="src/assets/img/home/sponsors/microsoft.svg" />
-              </a>
-          </li>
-          <li className={styles.listItem}>
-              <a className={styles.listItemLink} href="#">
-                  <img className={styles.listItemImage} src="src/assets/img/home/sponsors/onenorth.svg" />
-              </a>
-          </li>
-          <li className={styles.listItem}>
-              <a className={styles.listItemLink} href="#">
-                  <img className={styles.listItemImage} src="src/assets/img/home/sponsors/qualtrics.svg" />
-              </a>
-          </li>
-          <li className={styles.listItem}>
-              <a className={styles.listItemLink} href="#">
-                  <img className={styles.listItemImage} src="src/assets/img/home/sponsors/3red.svg" />
-              </a>
-          </li>
-          <li className={styles.listItem}>
-              <a className={styles.listItemLink} href="#">
-                  <img className={styles.listItemImage} src="src/assets/img/home/sponsors/johndeere.svg" />
-              </a>
-          </li>
-          <li className={styles.listItem}>
-              <a className={styles.listItemLink} href="#">
-                  <img className={styles.listItemImage} src="src/assets/img/home/sponsors/citadel.svg" />
-              </a>
-          </li>
-          <li className={styles.listItem}>
-              <a className={styles.listItemLink} href="#">
-                  <img className={styles.listItemImage} src="src/assets/img/home/sponsors/statefarm.svg" />
-              </a>
-          </li>
-          <li className={styles.listItem}>
-              <a className={styles.listItemLink} href="#">
-                  <img className={styles.listItemImage} src="src/assets/img/home/sponsors/1517.png" />
-              </a>
-          </li>
-          <li className={styles.listItem}>
-              <a className={styles.listItemLink} href="#">
-                  <img className={styles.listItemImage} src="src/assets/img/home/sponsors/akuna.png" />
-              </a>
-          </li>
-          <li className={styles.listItem}>
-              <a className={styles.listItemLink} href="#">
-                  <img className={styles.listItemImage} src="src/assets/img/home/sponsors/bmo.jpg" />
-              </a>
-          </li>
-          <li className={styles.listItem}>
-              <a className={styles.listItemLink} href="#">
-                  <img className={styles.listItemImage} src="src/assets/img/home/sponsors/bp.svg" />
-              </a>
-          </li>
-          <li className={styles.listItem}>
-              <a className={styles.listItemLink} href="#">
-                  <img className={styles.listItemImage} src="src/assets/img/home/sponsors/firebase.png" />
-              </a>
-          </li>
-          <li className={styles.listItem}>
-              <a className={styles.listItemLink} href="#">
-                  <img className={styles.listItemImage} src="src/assets/img/home/sponsors/github.png" />
-              </a>
-          </li>
-          <li className={styles.listItem}>
-              <a className={styles.listItemLink} href="#">
-                  <img className={styles.listItemImage} src="src/assets/img/home/sponsors/johndeere.svg" />
-              </a>
-          </li>
-          <li className={styles.listItem}>
-              <a className={styles.listItemLink} href="#">
-                  <img className={styles.listItemImage} src="src/assets/img/home/sponsors/namecheap.png" />
-              </a>
-          </li>
-          <li className={styles.listItem}>
-              <a className={styles.listItemLink} href="#">
-                  <img className={styles.listItemImage} src="src/assets/img/home/sponsors/techpoint.png" />
-              </a>
-          </li>
-          <li className={styles.listItem}>
-              <a className={styles.listItemLink} href="#">
-                  <img className={styles.listItemImage} src="src/assets/img/home/sponsors/twilio.svg" />
-              </a>
-          </li>
-          <li className={styles.listItem}>
-              <a className={styles.listItemLink} href="#">
-                  <img className={styles.listItemImage} src="src/assets/img/home/sponsors/viasat.svg" />
-              </a>
-          </li>
-          <li className={styles.listItem}>
-              <a className={styles.listItemLink} href="#">
-                  <img className={styles.listItemImage} src="src/assets/img/home/sponsors/yahoo.svg" />
-              </a>
-          </li>
-      </ul>
-      </div>
-    </div>
-  )
+
+      return (
+        <div className={styles.landing}>
+            <div className={styles.lanterns}>
+                <div className={styles.lanternUnit}></div>
+                <div className={styles.lanternUnit}></div>
+                <div className={styles.lanternUnit}></div>
+                <div className={styles.lanternUnit}></div>
+                <div className={styles.lanternUnit}></div>
+                <div className={styles.lanternUnit}></div>
+                <div className={styles.lanternUnit}></div>
+            </div>
+            <div className={styles.hero}>
+                <img className={styles.logo} src="src/assets/img/landing-logo.png"/>
+                <div className={styles.dateLabel}>February 24th - 26th</div>
+                <div className={styles.host}>
+                Co-hosted by <a href="http://fulcrum-gt.com/">Fulcrum</a>
+                </div>
+                <div className={styles.register}>
+                <Button className={styles.mb20} register={true} dest='/registration' label='REGISTER NOW' />
+                <div className={styles.login} > Already registered? <a href='/login'> Log in </a> </div>
+                </div>
+            </div>
+
+            <h4 className={styles.sponsorHeading}>Sponsors</h4>
+            <div className={styles.sponsorContainer}>
+            <ul className={styles.list}>
+                {sponsorList}
+            </ul>
+            </div>
+        </div>
+      );
+  }
 }
 
 
