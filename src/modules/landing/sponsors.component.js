b13db4c171297007fac7eb8338e388b0071d246c
Sujay Khandekar <khandek2@illinois.edu>
2017-02-18 14:45:54 -0600

    dynamically adding hrefs to the anchor tags

--- src/modules/landing/sponsors.component.js
+++ src/modules/landing/sponsors.component.js
@@ -6,55 +6,55 @@ import styles from './sponsors.scss'
 class Sponsors extends Component {
   render = () =>  {
       let sponsorUrls = [
-          "src/assets/img/home/sponsors/fulcum.svg",
-          "src/assets/img/home/sponsors/microsoft.svg",
-          "src/assets/img/home/sponsors/imo.svg",
-          "src/assets/img/home/sponsors/1517.png",
-          "src/assets/img/home/sponsors/3red.svg",
-          "src/assets/img/home/sponsors/a16z.png",
-          "src/assets/img/home/sponsors/akuna.png",
-          "src/assets/img/home/sponsors/amadeus.svg",
-          "src/assets/img/home/sponsors/aarp.png",
-          "src/assets/img/home/sponsors/att.svg",
-          "src/assets/img/home/sponsors/bmo.jpg",
-          "src/assets/img/home/sponsors/bp.svg",
-          "src/assets/img/home/sponsors/capitalone.svg",
-          "src/assets/img/home/sponsors/citadel.svg",
-          "src/assets/img/home/sponsors/cloudera.svg",
-          "src/assets/img/home/sponsors/facebook.svg",
-          "src/assets/img/home/sponsors/firebase.png",
-          "src/assets/img/home/sponsors/github.png",
-          "src/assets/img/home/sponsors/google.svg",
-          "src/assets/img/home/sponsors/huawei.svg",
-          "src/assets/img/home/sponsors/johndeere.svg",
-          "src/assets/img/home/sponsors/llnl.png",
-          "src/assets/img/home/sponsors/namecheap.png",
-          "src/assets/img/home/sponsors/onenorth.svg",
-          "src/assets/img/home/sponsors/qualtrics.svg",
-          "src/assets/img/home/sponsors/quikorder.jpeg",
-          "src/assets/img/home/sponsors/techpoint.jpg",
-          "src/assets/img/home/sponsors/soylent.png",
-          "src/assets/img/home/sponsors/statefarm.svg",
-          "src/assets/img/home/sponsors/twilio.png",
-          "src/assets/img/home/sponsors/viasat.svg",
-          "src/assets/img/home/sponsors/united.svg",
-          "src/assets/img/home/sponsors/yahoo.svg"
+          {logo: "src/assets/img/home/sponsors/fulcum.svg", site: "http\://fulcrum-gt.com/"},
+          {logo: "src/assets/img/home/sponsors/microsoft.svg", site: "#"},
+          {logo: "src/assets/img/home/sponsors/imo.svg", site: "#"},
+          {logo: "src/assets/img/home/sponsors/1517.png", site: "#"},
+          {logo: "src/assets/img/home/sponsors/3red.svg", site: "#"},
+          {logo: "src/assets/img/home/sponsors/a16z.png", site: "#"},
+          {logo: "src/assets/img/home/sponsors/akuna.png", site: "#"},
+          {logo: "src/assets/img/home/sponsors/amadeus.svg", site: "#"},
+          {logo: "src/assets/img/home/sponsors/aarp.png", site: "#"},
+          {logo: "src/assets/img/home/sponsors/att.svg", site: "#"},
+          {logo: "src/assets/img/home/sponsors/bmo.jpg", site: "#"},
+          {logo: "src/assets/img/home/sponsors/bp.svg", site: "#"},
+          {logo: "src/assets/img/home/sponsors/capitalone.svg", site: "#"},
+          {logo: "src/assets/img/home/sponsors/citadel.svg", site: "#"},
+          {logo: "src/assets/img/home/sponsors/cloudera.svg", site: "#"},
+          {logo: "src/assets/img/home/sponsors/facebook.svg", site: "#"},
+          {logo: "src/assets/img/home/sponsors/firebase.png", site: "#"},
+          {logo: "src/assets/img/home/sponsors/github.png", site: "#"},
+          {logo: "src/assets/img/home/sponsors/google.svg", site: "#"},
+          {logo: "src/assets/img/home/sponsors/johndeere.svg", site: "#"},
+          {logo: "src/assets/img/home/sponsors/llnl.png", site: "#"},
+          {logo: "src/assets/img/home/sponsors/namecheap.png", site: "#"},
+          {logo: "src/assets/img/home/sponsors/onenorth.svg", site: "#"},
+          {logo: "src/assets/img/home/sponsors/qualtrics.svg", site: "#"},
+          {logo: "src/assets/img/home/sponsors/quikorder.jpeg", site: "#"},
+          {logo: "src/assets/img/home/sponsors/techpoint.jpg", site: "#"},
+          {logo: "src/assets/img/home/sponsors/quikorder.jpeg", site: "#"},
+          {logo: "src/assets/img/home/sponsors/soylent.png", site: "#"},
+          {logo: "src/assets/img/home/sponsors/statefarm.svg", site: "#"},
+          {logo: "src/assets/img/home/sponsors/twilio.png", site: "#"},
+          {logo: "src/assets/img/home/sponsors/viasat.svg", site: "#"},
+          {logo: "src/assets/img/home/sponsors/united.svg", site: "#"},
+          {logo: "src/assets/img/home/sponsors/yahoo.svg", site: "#"}
       ];
-    
-      let sponsorList = sponsorUrls.map((url, key) => {
+
+      let sponsorList = sponsorUrls.map((obj, key) => {
             let itemStyle = styles.listItem;
-            if (url.includes('fulcum')) {
+            if (obj.logo.includes('fulcum')) {
                 itemStyle = styles.topListItem
             }
 
-            if (url.includes('microsoft') || url.includes('imo')) {
+            if (obj.logo.includes('microsoft') || obj.logo.includes('imo')) {
                 itemStyle = styles.secondListItem;
             }
 
             return (
                 <li className={itemStyle} key={key}>
-                    <a className={styles.listItemLink} href="#">
-                        <img className={styles.listItemImage} src={url} />
+                    <a className={styles.listItemLink} href={obj.site}>
+                        <img className={styles.listItemImage} src={obj.logo} />
                     </a>
                 </li>
             );
