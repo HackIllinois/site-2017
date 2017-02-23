0c436f4468d4866360d18d10cc78b47de956968b
Simon Lu <lusimon95@gmail.com>
2017-02-23 15:13:02 -0600

    Prize panels added and styled; content yet to be finished

---
+++ src/modules/prizes/components/google.component.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+import styles from './prize-panel.scss'
+
+class Google extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            showData: false
+        };
+        this.display = this.display.bind(this);
+    }
+
+    display = () => {
+        this.setState({showData: !this.state.showData});
+    };
+
+    render = () => (
+
+        <div className={styles['prizes-body-panel']}>
+            <div onClick={this.display} className={styles['prizes-body-panel-header-1']}>
+                <h4 className={styles['prizes-body-panel-title']}>Google</h4>
+                <img className={styles['prizes-body-caret']} src="src/assets/img/down-arrow.svg"/>
+            </div>
+
+            { this.state.showData ? <div id="content-1" className={styles['prizes-body-panel-content']}>
+                <br />
+                <h5 className={styles['prizes-body-panel-content-title']}>Prize Name</h5>
+                <h6 className={styles['prizes-body-panel-content-subtitle']}>Prize Description</h6>
+                <br />
+            </div> : null }
+        </div>
+
+    )
+}
+
+export default Google
--- src/modules/prizes/prizes.component.js
+++ src/modules/prizes/prizes.component.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 
+import One517 from './components/1517.component'
+import AARP from './components/aarp.component'
+import Amadeus from './components/amadeus.component'
+import CapitalOne from './components/capitalone.component'
+import Facebook from './components/facebook.component'
+import Firebase from './components/firebase.component'
+import Fulcrum from './components/fulcrum.component'
+import Google from './components/google.component'
+import IMO from './components/imo.component'
+import JohnDeere from './components/johndeere.component'
+import MakeSchool from './components/makeschool.component'
+import Microsoft from './components/microsoft.component'
+import Twilio from './components/twilio.component'
+
 import styles from './prizes.scss'
 
 class Prizes extends Component {
@@ -10,13 +24,25 @@ class Prizes extends Component {
 
                 <img className={styles['prizes-header-logo']} src="src/assets/img/svg/landing-logo.svg" /> <br/><br />
                 <h3 className={styles['prizes-header-title']}>Prizes</h3>
-                <h5 className={styles['prizes-header-subtitle']}>Placeholder Text</h5>
+                <h5 className={styles['prizes-header-subtitle']}>The following prizes will be awarded at HackIllinois 2017:</h5>
                 <br />
 
             </div>
 
             <div className={styles['prizes-body']}>
-                
+                <Fulcrum />
+                <Microsoft />
+                <IMO />
+                <AARP />
+                <Google />
+                <Firebase />
+                <Amadeus />
+                <Facebook />
+                <JohnDeere />
+                <CapitalOne />
+                <Twilio />
+                <One517 />
+                <MakeSchool />
             </div>
             
             <br />
