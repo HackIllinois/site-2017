cfc1ed77b1c1b65b5d8ab976377eecdd39b9034f
Radhir Kothuri <kothuri2@illinois.edu>
2017-02-23 22:13:22 -0600

    fix fulcrum prize

--- src/modules/prizes/components/fulcrum.component.js
+++ src/modules/prizes/components/fulcrum.component.js
@@ -21,16 +21,15 @@ class Fulcrum extends Component {
 
         <div className={styles['prizes-body-panel']}>
             <div id={styles.header1} onClick={this.display} className={styles['prizes-body-panel-header-1']}>
-                <h4 className={styles['prizes-body-panel-title']}>One More Hour In The Day</h4>
+                <h4 className={styles['prizes-body-panel-title']}>Don't Discard The Human</h4>
                 <img className={styles['prizes-body-caret']} src="src/assets/img/down-arrow.svg"/>
             </div>
 
             { this.state.showData ? <div id="content-1" className={styles['prizes-body-panel-content']}>
                 <br />
                 <h5 className={styles['prizes-body-panel-content-subtitle']}>
-                    Life is too short to be doing the repetitive. This category is for open source technology that
-                    promotes efficiency and enables people to do more with their time. Imagine...
-                    One More Hour In The Day.
+                    Tech that brings about efficiency and requires human participation to achieve more meaningful, authentic, otherwise harder to replicate solutions.
+                    'Best Automation with a Human-Centric Component'.
                     <br /><br />
                 </h5>
                     <h4>Presented by <a href="http://fulcrum-gt.com/">Fulcrum-GT</a>.</h4>
